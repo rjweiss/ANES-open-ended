abbd648586e5e06f10a3cb2156ea87060e67d23c
Rebecca Weiss <rjweiss@stanford.edu>
2012-05-12 13:33:08 -0700

    trying different N of clusters

--- unsupervised.R
+++ unsupervised.R
@@ -13,8 +13,8 @@ data.corpus <- tm_map(data.corpus, tolower)
 #data.corpus <- tm_map(data.corpus, stem <- Document)
 
 data.dtm <- DocumentTermMatrix(data.corpus,
-  		control = list(weighting = weightTfIdf,
-						   stopwords = TRUE))
+                               control = list(weighting = weightTfIdf,
+                                              stopwords = TRUE))
 
 data.dtm.mat <- unique(as.matrix(data.dtm))
 
@@ -24,16 +24,15 @@ cosine.dist <- function(p, q){
 
 cosdist.mat <- matrix(NA, nrow=nrow(data.dtm.mat), ncol=nrow(data.dtm.mat))
 
-
 for (j in 1:nrow(data.dtm.mat)){
   for (k in 1:j){
-		cosdist.mat[j,k] <- cosdist.mat[k,j] <- cosine.dist(data.dtm.mat[j,], data.dtm.mat[k,])
-	}}
+    cosdist.mat[j,k] <- cosdist.mat[k,j] <- cosine.dist(data.dtm.mat[j,], data.dtm.mat[k,])
+  }}
 
 for(j in 1:nrow(cosdist.mat)){
   for(k in 1:j){
-		cosdist.mat[j,k]<- cosdist.mat[k,j]<- cosdist.mat[k,j] + runif(1, min=0, max=1e-6)
-	}
+    cosdist.mat[j,k]<- cosdist.mat[k,j]<- cosdist.mat[k,j] + runif(1, min=0, max=1e-6)
+  }
 }
 
 diag(cosdist.mat) <- 0
@@ -47,12 +46,12 @@ cos.mds <- cmdscale(cosdist.mat)
 colnames(cos.mds) = c("x","y")
 
 fig <- ggplot(data = as.data.frame(cos.mds), aes(
-  		x = x,
-			y = y,
-			label = row.names(data))) +
-	geom_point(aes(alpha = 0.5)) +
-	opts(legend.position = "none")
-	ggsave(fig, file= "/Users/Rebecca/Dropbox/research/ANES/plots/p_cos_mds.pdf")
+  x = x,
+  y = y,
+  label = row.names(data))) +
+    geom_point(aes(alpha = 0.5)) +
+    opts(legend.position = "none")
+ggsave(fig, file= "/Users/Rebecca/Dropbox/research/ANES/plots/p_cos_mds.pdf")
 
 cos.sam <- sammon(cosdist.mat)
 
@@ -60,11 +59,11 @@ cos.sam <- as.data.frame(cos.sam$points)
 colnames(cos.sam) = c("x","y")
 
 fig <- ggplot(data = cos.sam, aes(
-  					x = x,
-						y = y,
-						label = row.names(data))) +
-		geom_point(aes(alpha = 0.5)) +
-		opts(legend.position = "none")
+  x = x,
+  y = y,
+  label = row.names(data))) +
+    geom_point(aes(alpha = 0.5)) +
+    opts(legend.position = "none")
 ggsave(fig, file= "/Users/Rebecca/Dropbox/research/ANES/plots/p_cos_sam.pdf")
 
 
@@ -78,7 +77,7 @@ nclust = 8
 m1 <- kmeans(cosdist.mat, centers = nclust, iter.max = 10)
 #m2 <- multmixEM(data.dtm.mat, k = nclust, lambda=NULL, theta=NULL, maxit=1000, epsilon=1e-08, verb=FALSE)
 
-c <- apply(m2$posterior, 1, which.max)
+#c <- apply(m2$posterior, 1, which.max)
 
 #justin grimmer's code for monroe, colaresi, and quinn "lexical feature selection"
 fightin <- function(clust.num, clustering, data){
